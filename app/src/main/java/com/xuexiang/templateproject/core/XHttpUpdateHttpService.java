d3d40e452e70f22dd8bfd994c32dc40a8950e490
xuexiangjys <xuexiangjys@163.com>
2019-05-14 08:48:07 +0800

    增加copyright

--- app/src/main/java/com/xuexiang/templateproject/core/XHttpUpdateHttpService.java
+++ app/src/main/java/com/xuexiang/templateproject/core/XHttpUpdateHttpService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 xuexiangjys(xuexiangjys@163.com)
+ * Copyright (C) 2019 xuexiangjys(xuexiangjys@163.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
 package com.xuexiang.templateproject.core;
