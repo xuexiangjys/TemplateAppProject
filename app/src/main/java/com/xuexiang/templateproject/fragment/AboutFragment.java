e5f3d7ea864f213bb466096bbf82210d0f5b52dc
xuexiangjys <xuexiangjys@163.com>
2019-10-31 00:15:05 +0800

    优化模版内容

--- app/src/main/java/com/xuexiang/templateproject/fragment/AboutFragment.java
+++ app/src/main/java/com/xuexiang/templateproject/fragment/AboutFragment.java
@@ -17,9 +17,21 @@
 
 package com.xuexiang.templateproject.fragment;
 
+import android.view.View;
+import android.widget.TextView;
+
 import com.xuexiang.templateproject.R;
 import com.xuexiang.templateproject.core.BaseFragment;
+import com.xuexiang.templateproject.core.webview.AgentWebActivity;
 import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xui.widget.grouplist.XUIGroupListView;
+import com.xuexiang.xutil.app.AppUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.BindView;
 
 /**
  * @author xuexiang
@@ -28,6 +40,13 @@ import com.xuexiang.xpage.annotation.Page;
 @Page(name = "关于")
 public class AboutFragment extends BaseFragment {
 
+    @BindView(R.id.version)
+    TextView mVersionTextView;
+    @BindView(R.id.about_list)
+    XUIGroupListView mAboutGroupListView;
+    @BindView(R.id.copyright)
+    TextView mCopyrightTextView;
+
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_about;
@@ -35,6 +54,16 @@ public class AboutFragment extends BaseFragment {
 
     @Override
     protected void initViews() {
+        mVersionTextView.setText(String.format("版本号：%s", AppUtils.getAppVersionName()));
 
+        XUIGroupListView.newSection(getContext())
+                .addItemView(mAboutGroupListView.createItemView(getResources().getString(R.string.about_item_homepage)), v -> AgentWebActivity.goWeb(getContext(), getString(R.string.url_project_github)))
+                .addItemView(mAboutGroupListView.createItemView(getResources().getString(R.string.about_item_author_github)), v -> AgentWebActivity.goWeb(getContext(), getString(R.string.url_author_github)))
+                .addItemView(mAboutGroupListView.createItemView(getResources().getString(R.string.about_item_add_qq_group)), v -> AgentWebActivity.goWeb(getContext(), getString(R.string.url_add_qq_group)))
+                .addTo(mAboutGroupListView);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy", Locale.CHINA);
+        String currentYear = dateFormat.format(new Date());
+        mCopyrightTextView.setText(String.format(getResources().getString(R.string.about_copyright), currentYear));
     }
 }
