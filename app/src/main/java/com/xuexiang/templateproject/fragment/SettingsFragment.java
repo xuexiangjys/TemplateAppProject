8f2e609733fbfb8cdeb04cf4c6aef8c37580193e
xuexiangjys <xuexiangjys@163.com>
2020-03-05 12:49:27 +0800

    更新内容

--- app/src/main/java/com/xuexiang/templateproject/fragment/SettingsFragment.java
+++ app/src/main/java/com/xuexiang/templateproject/fragment/SettingsFragment.java
@@ -19,14 +19,32 @@ package com.xuexiang.templateproject.fragment;
 
 import com.xuexiang.templateproject.R;
 import com.xuexiang.templateproject.core.BaseFragment;
+import com.xuexiang.templateproject.utils.XToastUtils;
+import com.xuexiang.xaop.annotation.SingleClick;
 import com.xuexiang.xpage.annotation.Page;
+import com.xuexiang.xui.widget.textview.supertextview.SuperTextView;
+
+import butterknife.BindView;
 
 /**
  * @author xuexiang
  * @since 2019-10-15 22:38
  */
 @Page(name = "设置")
-public class SettingsFragment extends BaseFragment {
+public class SettingsFragment extends BaseFragment implements SuperTextView.OnSuperTextViewClickListener {
+
+    @BindView(R.id.menu_common)
+    SuperTextView menuCommon;
+    @BindView(R.id.menu_privacy)
+    SuperTextView menuPrivacy;
+    @BindView(R.id.menu_push)
+    SuperTextView menuPush;
+    @BindView(R.id.menu_helper)
+    SuperTextView menuHelper;
+    @BindView(R.id.menu_change_account)
+    SuperTextView menuChangeAccount;
+    @BindView(R.id.menu_logout)
+    SuperTextView menuLogout;
 
     @Override
     protected int getLayoutId() {
@@ -35,6 +53,30 @@ public class SettingsFragment extends BaseFragment {
 
     @Override
     protected void initViews() {
+        menuCommon.setOnSuperTextViewClickListener(this);
+        menuPrivacy.setOnSuperTextViewClickListener(this);
+        menuPush.setOnSuperTextViewClickListener(this);
+        menuHelper.setOnSuperTextViewClickListener(this);
+        menuChangeAccount.setOnSuperTextViewClickListener(this);
+        menuLogout.setOnSuperTextViewClickListener(this);
+    }
 
+    @SingleClick
+    @Override
+    public void onClick(SuperTextView superTextView) {
+        switch(superTextView.getId()) {
+            case R.id.menu_common:
+            case R.id.menu_privacy:
+            case R.id.menu_push:
+            case R.id.menu_helper:
+                XToastUtils.toast(superTextView.getLeftString());
+                break;
+            case R.id.menu_change_account:
+            case R.id.menu_logout:
+                XToastUtils.toast(superTextView.getCenterString());
+                break;
+            default:
+                break;
+        }
     }
 }
