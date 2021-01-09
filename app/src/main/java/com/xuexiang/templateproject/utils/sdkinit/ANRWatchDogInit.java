90f44fd0a01bca2232fef5e69568a6c56d91e55e
xuexiangjys <xuexiangjys@163.com>
2021-01-10 00:18:46 +0800

    去除无用的资源

--- app/src/main/java/com/xuexiang/templateproject/utils/sdkinit/ANRWatchDogInit.java
+++ app/src/main/java/com/xuexiang/templateproject/utils/sdkinit/ANRWatchDogInit.java
@@ -70,7 +70,7 @@ public final class ANRWatchDogInit {
             }
             //当返回是0或者负数时，就会触发ANR监听回调
             return ret;
-        }).setANRListener(CUSTOM_LISTENER).start();
+        }).setANRListener(SILENT_LISTENER).start();
     }
 
     public static ANRWatchDog getANRWatchDog() {
